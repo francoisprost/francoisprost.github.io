06dee45ad1a42a6714050505f08f63576ea40f61
francoisprost <francoisdelyon3@hotmail.fr>
2024-03-14 23:23:07 +0100

    Navbar effect

--- script.js
+++ script.js
@@ -7,3 +7,18 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         });
     });
 });
+
+
+// When the user scrolls down 50px from the top of the document, resize the header's font size
+window.onscroll = function() {
+    growShrinkLogo()
+  };
+  
+  function growShrinkLogo() {
+    var Logo = document.getElementById("logo")
+    if (document.body.scrollTop > 5 || document.documentElement.scrollTop > 5) {
+      Logo.style.width = '30px';
+    } else {
+      Logo.style.width = '60px';
+    }
+  }
